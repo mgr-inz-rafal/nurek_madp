3c9e374161885035033f78f095e8221489a44ed7
mgr-inz-rafal <rchabowski@gmail.com>
2025-05-04 17:42:08 +0200

    Chuj movement

--- build.cmd
+++ build.cmd
@@ -2,3 +2,4 @@ del nurek.a65
 del nurek.exe
 mp.exe nurek.pas -ipath:d:\Sync\Atari\Narzedzia\madpascal\lib
 mads.exe nurek.a65 -x -i:d:\Sync\Atari\Narzedzia\madpascal\base -o:nurek.xex -l:nurek.lst -t:nurek.lab 
+nurek.xex
--- nurek.pas
+++ nurek.pas
@@ -1,4 +1,4 @@
-uses crt, graph;
+uses crt, graph, math;
 
 const
   NurekData: array[0..607] of BYTE = (
@@ -36,21 +36,66 @@ const
     0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,0,0
   );
 
+type
+  ChujMoveOutcome = (Nothing);
+
+type
+  Game = Object
+    chuj_x: BYTE;
+    chuj_y: BYTE;
+    chuj_a: SINGLE;
+    finish: BOOLEAN;
+    constructor Build;
+    function MoveChuj: ChujMoveOutcome;
+    procedure DrawChuj;
+  end;
+
+constructor Game.Build();
+begin
+  chuj_x := 139;
+  chuj_y := 62;
+  chuj_a := DegToRad(single(270));
+  finish := FALSE;
+end;  
+
+procedure Game.DrawChuj;
+begin
+  PutPixel(chuj_x, chuj_y, 1);
+end;
+
+function Game.MoveChuj: ChujMoveOutcome;
+begin
+  chuj_x := Byte(Round(single(chuj_x) + sin(chuj_a)));
+  chuj_y := Byte(Round(single(chuj_y) + cos(chuj_a)));
+
+  Result := ChujMoveOutcome(Nothing);
+end;
+
 procedure DrawNurek;
 var
-  i, j: Integer;
+  i, j: BYTE;
 begin
   for j := 0 to 31 do
     for i := 0 to 18 do
       PutPixel(i+140, j+40, NurekData[j * 19 + i]);
 end;
 
+var
+  g: Game;
+
 begin
 
   InitGraph(7);
+
+  g.Build;
+
   DrawNurek;
 
-  ReadKey;
+  while not g.finish do
+  begin
+    g.DrawChuj;
+    g.MoveChuj;
+  end;
 
 end.
 
